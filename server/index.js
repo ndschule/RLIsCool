19d8e03fbe82a1b979a256d2dd267db923b6c586
ndschule <78383622+ndschule@users.noreply.github.com>
2021-05-21 22:17:47 -0400

    Tutorial 2 Complete.

--- server/index.js
+++ server/index.js
@@ -1,34 +1,49 @@
 const express = require('express');
 const ejs = require('ejs');
 const path = require('path');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+
+// Navigation
 
 const clientPath = path.join(__dirname,'../client/');
 const staticPath = path.join(clientPath,'/static/');
 const viewsPath = path.join(clientPath,'/views/');
 
+// Basic Server
+
 const app = express();
+app.use(express.static(staticPath));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(session({
+    name: 'rockets',
+    secret: 'octaneisbestcar',
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        maxAge: 1000*60*60*24*3,
+    }
+}));
+
+app.listen(2000);
+
+// Setting Views
 
 app.set('view engine','ejs');
 app.set('views',viewsPath);
 
-var x = 0;
+// Routes
 
-const counter = function(req, res, next) {
-    x++;
-    console.log(x);
-    next();
-}
-
-app.use(express.static(staticPath));
 
 app.get('/', function(req, res) {
-    res.render('index');
+    res.render('index', {nomen: req.session.username});
 });
 
-app.get('/famous', counter, function(req, res) {
-    res.render('famous',{count: x});
+app.get('/famous', function(req, res) {
+    res.render('famous', {nomen: req.session.username});
 });
 
-
-
-app.listen(2000);
+app.post('/welcome', (req, res) => {
+    req.session.username=req.body.visitorname;
+    res.redirect('/');
+});
